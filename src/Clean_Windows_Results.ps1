8d37132abc4a83b9a13d971b5307bab33330361c
HunterAP <etgarkaspi@gmail.com>
2021-05-23 17:13:37 -0400

    Consolidated Windows ccleaning/compilaton scripts

--- src/Clean_Windows_Results.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "Cleaning up *.txt objects in Compiled folder"
-
-Remove-Item -Path "./files_runs" -Include *.txt -Recurse -Force
--- src/Compile_Windows.ps1
+++ src/Compile_Windows.ps1
@@ -1,32 +1,46 @@
-# Write-Output "Cleaning up *.c objects"
-# Get-ChildItem -Path "." -File -Recurse -Filter *.c | Foreach-Object {
-#     Write-Output $_.Name
-#     rm $_.FullName
-# }
-#
-# Write-Output "Cleaning up *.pyd objects"
-# Get-ChildItem -Path "." -File -Recurse -Filter *.pyd | Foreach-Object {
-#     Write-Output $_.Name
-#     rm $_.FullName
-# }
+Write-Output "Cleaning up *.c objects"
+Get-ChildItem -Path "." -File -Recurse -Filter *.c | Foreach-Object {
+    Write-Output $_.Name
+    rm $_.FullName
+}
 
-# Write-Output "Cleaning up *.txt objects in Compiled folder"
-# Get-ChildItem -Path "Primes/Optimized" -File -Recurse -Filter *.txt | Foreach-Object {
-#     Write-Output $_.Name
-#     # rm $content
-# }
+Write-Output "Cleaning up *.pyc objects"
+Get-ChildItem -Path "." -File -Recurse -Filter *.pyc | Foreach-Object {
+    Write-Output $_.Name
+    rm $_.FullName
+}
+
+Write-Output "Cleaning up *.pyd objects"
+Remove-Item -Path "./build" -Include *.pyd -Recurse -Force
+Get-ChildItem -Path "." -File -Recurse -Filter *.pyd | Foreach-Object {
+    Write-Output $_.Name
+    rm $_.FullName
+}
+
+Write-Output "Cleaning up *.so objects"
+Get-ChildItem -Path "." -File -Recurse -Filter *.so | Foreach-Object {
+    Write-Output $_.Name
+    rm $_.FullName
+}
+
+Write-Output "Cleaning up *.txt files"
+Remove-Item -Path "./files_runs" -Include *.txt -Recurse -Force
+Get-ChildItem -Path "." -File -Recurse -Filter *.txt | Foreach-Object {
+    Write-Output $_.Name
+    rm $_.FullName
+}
 
 # Compile
 Write-Output "Compiling Cython objects"
 Get-ChildItem -Path "Primes/Compiled" -File -Recurse -Filter Compiler_*.py | Foreach-Object {
      Write-Output $_.FullName
-     python37 $_.FullName build_ext --inplace
+     python $_.FullName build_ext --inplace
 }
 
 Write-Output "Compiling Optimized objects"
 Get-ChildItem -Path "Primes/Optimized" -File -Recurse -Filter Compiler_*.py | Foreach-Object {
      Write-Output $_.FullName
-     python37 $_.FullName build_ext --inplace
+     python $_.FullName build_ext --inplace
 }
 
 # python37 "Compiler_Tester.py" build_ext --inplace
